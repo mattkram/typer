f77baf66e56b2fe6a73f17256ed4dac231573ca5
mattkram <mkramer@anaconda.com>
2024-04-09 10:13:28 -0500

    Add failing test to reproduce bug

--- tests/test_rich_utils.py
+++ tests/test_rich_utils.py
@@ -36,3 +36,15 @@ def test_rich_utils_click_rewrapp():
     assert "Hello World" in result.stdout
     result = runner.invoke(app, ["secondary"])
     assert "Hello Secondary World" in result.stdout
+
+
+def test_rich_utils_no_error_no_commands():
+    app = typer.Typer(rich_markup_mode="markdown")
+
+    @app.callback(no_args_is_help=True)
+    def main():
+        """Some main callback"""
+
+    result = runner.invoke(app, ["--help"])
+    assert result.exit_code == 0
+    assert "Some main callback" in result.stdout
